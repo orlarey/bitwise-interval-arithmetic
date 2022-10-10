dc6ceaa9a69eda77ea39bc5c17ae5209159c7039
Yann Orlarey <orlarey@gmail.com>
2022-10-10 11:56:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![C/C++ CI](https://github.com/orlarey/cpptemplate/workflows/C/C++%20CI/badge.svg)
+
+# Hello World
+ Basic C++ project.
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+void hello()
+{
+    std::cout << "hello world" << std::endl;
+}
+
+int main()
+{
+    hello();
+    return 0;
+}
